2efcceba5955fb8fc362f3ce1cd2d82e5dc1efac
kolestom <88550454+kolestom@users.noreply.github.com>
2022-12-14 10:26:15 +0100

    almost ready

--- script.js
+++
@@ -1,8 +0,0 @@
-let text = document.getElementById('hello')
-// let button = getElementById('button')
-
-const changeText = () => {
-    text.innerText = 'World'
-}
-
-text.addEventListener('click', changeText)
